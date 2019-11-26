d51dcb45b7a5f6b880c6c5cc7c10af265a0f1cde
Frankelycp <frankelycp@gmail.com>
2019-11-26 14:16:38 -0300

    23

---
+++ app/src/main/java/com/g/gomeria/Clases/ConfS.java
@@ -0,0 +1,19 @@
+package com.g.gomeria.Clases;
+
+public class ConfS {
+
+    private String confs ;
+
+
+    public String getConfs()
+    {
+        return confs;
+    }
+
+    public void setConfs(String confs)
+    {
+        this.confs = confs;
+    }
+
+
+}
---
+++ app/src/main/java/com/g/gomeria/Clases/ConfT.java
@@ -0,0 +1,19 @@
+package com.g.gomeria.Clases;
+
+public class ConfT {
+
+    private String conft ;
+
+
+    public String getConft()
+    {
+        return conft;
+    }
+
+    public void setConft(String conft)
+    {
+        this.conft = conft;
+    }
+
+
+}
